a7ae7dfbaa813e95f68566865f02391e93ec54ba
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-05 23:32:18 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,11 @@ class SessionsController < ApplicationController
 
   def create
     @resp = Faradat.get(https://foursquare.com/oauth2/access_token) do |req|
-      req.params["client_id"] = env["FOURSQUARE_CLIENT_ID"]
-      req.params["client_secret"] = YOUR_CLIENT_SECRET
+      req.params["client_id"] = ENV["FOURSQUARE_CLIENT_ID"]
+      req.params["client_secret"] = ENV["FOURSQUARE_SECRET"]
       req.params["grant_type"] = authorization_code
       req.params["redirect_uri"] = YOUR_REGISTERED_REDIRECT_URI
-      req.params["code"] = CODE)
+      req.params["code"] = params[:code]
     end
   end
 end
