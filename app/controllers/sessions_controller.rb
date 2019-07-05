cd1965b60ab02ac5370e80a46af621ecd4472c78
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-05 23:30:39 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,11 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user, only: :create
 
   def create
-    @resp = Faradat.get()
+    @resp = Faradat.get(https://foursquare.com/oauth2/access_token) do |req|
+    req.params["client_id"] = YOUR_CLIENT_ID
+    req.params["client_secret"] = YOUR_CLIENT_SECRET
+    req.params["grant_type"] = authorization_code
+    req.params["redirect_uri"] = YOUR_REGISTERED_REDIRECT_URI
+    req.params["code"] = CODE)
   end
 end
